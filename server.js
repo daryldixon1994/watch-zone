1cf0779d09bf4068ce1b65c3431f33bd283b0ba6
daryldixon1994 <kmohamedanouar@gmail.com>
2024-07-01 10:04:15 +0100

    configure CORS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 // constants
@@ -10,6 +11,11 @@ mongoose
   .then(() => console.log("connected to database 🚀"))
   .catch((err) => console.log("❌", err));
 // middlewares
+let corsOptions = {
+  origin: "http://localhost:3000/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 // routes
 app.use("/api/customer", require("./routes/customer"));
